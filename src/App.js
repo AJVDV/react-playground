cd98270a3c7200de82ed16932cafb4f942f7b8be
Andrew VanDerVeen <69212799+AJVDV@users.noreply.github.com>
2020-10-22 15:37:06 -0700

    Initial Commit, all previous updates done locally
    All current implementations provided here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+import React, { Component } from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+    ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip> 
+)
+
+//import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello world</h2>
+        <HelloWorld />
+        <h2>Bomb</h2>
+        <Bomb />
+        <h2>Roulette Gun</h2>
+        <RouletteGun bulletInChamber={6} />
+      </div>
+    );
+  }
 }
 
+
 export default App;
+/*function App() {
+  return (
+    <main className='App'>
+      <Split className='left' flexBasis={2}>
+        This is the content for the left `Split`. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+        {/* make another toolitp directly inside the App *///}
+/*        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content fot the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  );
+} */
+
+//export default App;
